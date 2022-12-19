75855b9f9710c5394cf0f03950fea42e0b157094
daniel <daniel.bnh13@gmail.com>
2022-12-19 13:40:36 -0300

    docs: css do cartao terminado com a rota para api parcialmente terminada

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,126 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+import Cards from 'react-credit-cards';
+
+import 'react-credit-cards/es/styles-compiled.css';
+import {
+  Label,
+  TicketInfo,
+  CreditCardContainer,
+  Button,
+  PaymentConfirmation,
+} from '../../../components/Dashboard/Payment/index.js';
+
+import InputMask from 'react-input-mask';
+import { useState } from 'react';
+import UserContext from '../../../contexts/UserContext.js';
+import { useContext } from 'react';
+import { FcOk } from 'react-icons/fc';
+import { useEffect } from 'react';
+import useToken from '../../../hooks/useToken';
+import * as paymentApi from '../../../services/paymentApi';
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const { userData, setUserData } = useContext(UserContext);
+  const [cvc, setCvc] = useState('');
+  const [expiry, setExpiry] = useState('');
+  const [focus, setFocus] = useState('');
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const token = useToken();
+  async function submit(e) {
+    e.preventDefault();
+    try {
+      const body={
+        ticketId: 0,
+        issuer: 'issuer',
+        number: number,
+        name: name,
+        expirationDate: expiry,
+        cvv: cvc
+      };
+      alert(body.issuer);
+      await paymentApi.pay(body, token);
+      setUserData({ ...userData, isTickedPayed: true });
+    } catch {
+      alert('erro');
+    }
+  }
+  return (
+    <>
+      <TicketInfo width={'290px'} height={'108px'}>
+        <p>
+          {userData.ticketModality} + {userData.ticketAccommodation}
+        </p>
+        <p>R$ {userData.ticketPrice}</p>
+      </TicketInfo>
+      <Label marginTop={'44px'}>Pagamento</Label>
+      {userData.isTickedPayed ? (
+        <PaymentConfirmation>
+          <FcOk color={'#36B853'} size={'40px'} style={{ marginRight: '14px' }} />
+          <div>
+            <p>Pagamento confirmado!</p>
+            <p>Prossiga para escolha de hospedagem e atividades</p>
+          </div>
+        </PaymentConfirmation>
+      ) : (
+        <>
+          <CreditCardContainer>
+            <Cards cvc={cvc} expiry={expiry} focused={focus} name={name} number={number} />
+            <form onSubmit={submit}>
+              <InputMask
+                type={'tel'}
+                name={'number'}
+                placeholder="Card Number"
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+                onFocus={(e) => setFocus(e.target.name)}
+                pattern="\d\d\d\d \d\d\d\d \d\d\d\d \d\d\d\d"
+                mask="9999 9999 9999 9999"
+                maskChar={'*'}
+                required
+              ></InputMask>
+              <p>E.g.: 49..., 51..., 36..., 37...</p>
+              <input
+                type={'tel'}
+                name={'name'}
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                onFocus={(e) => setFocus(e.target.name)}
+                required
+              />
+              <div>
+                <InputMask
+                  type={'tel'}
+                  name={'expiry'}
+                  placeholder="Valid Thru"
+                  value={expiry}
+                  onChange={(e) => setExpiry(e.target.value.replace(' / ', ''))}
+                  onFocus={(e) => setFocus(e.target.name)}
+                  pattern="\d\d / \d\d"
+                  mask="99 / 99"
+                  maskChar=""
+                  required
+                ></InputMask>
+                <input
+                  type="tel"
+                  name="cvc"
+                  placeholder="CVC"
+                  value={cvc}
+                  onChange={(e) => setCvc(e.target.value)}
+                  onFocus={(e) => setFocus(e.target.name)}
+                  pattern="\d{3,4}"
+                  maxLength={3}
+                  required
+                />
+              </div>
+              <Button type={'submit'} marginTop={'60px'}>
+                 FINALIZAR PAGAMENTO
+              </Button>
+            </form>
+          </CreditCardContainer>
+        </>
+      )}
+    </>
+  );
 }
---
+++ src/services/paymentApi.js
@@ -0,0 +1,12 @@
+import api from './api';
+
+export async function pay(body, token) {
+  const response = await api.post('/payments/process', body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  
+  return response.data;
+};
+
