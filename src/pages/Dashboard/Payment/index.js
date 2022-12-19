bb0e851d4191857b4402579445bad1b190f6ee78
PauloGabrieel <gabrieeel.c@gmail.com>
2022-12-19 20:11:17 -0300

    feat: reserva de ingresso com hospedagem ou sem hospedagem

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,33 +1,56 @@
 import useTicket from '../../../hooks/api/useGetTicketTypes';
-import { useEffect, useState } from 'react';
+import useReservation from '../../../hooks/api/useReservation';
+import { useState }  from 'react';
+import { toast } from 'react-toastify';
 import { TicketPrice, TicketType, Card, TicketTypeBox, SubTitle, StyledTypography, Button } from '../../../components/Payment';
 
 export default function Payment() {
   const [presential, setPresential] = useState(false);
+  const [ presentialWithoutHotel, setPresentialWihtoutHotel ] = useState(false);
+  const [presentialWithHotel, setPresentialWihtHotel] = useState(false);
   const [online, setOnline] = useState(false);
-  const [Tickets, setTickets ] = useState({}); 
-  const { ticket, ticketLoading } = useTicket();
+  const [Ticket, setTicket ] = useState(); 
+  const { postTicket, } = useReservation();
+  const { ticket } = useTicket();
   
   function selectTicket(type) {
-    if(type === 'presential') {
+    let ticketType = [];
+    if(type === 'no hotel') {
+      setPresentialWihtoutHotel(!presentialWithoutHotel);
+      setPresentialWihtHotel(false);
+      ticketType = ticket.filter(item => item.name === 'presencial' && item.includesHotel === false );
+      setTicket(ticketType);
+      return;
+    }
+    
+    if(type === 'presencial') {
       setPresential(!presential);
-    } else {
+      ticketType = ticket.filter(item => item.name === type && item.includesHotel === false );
+    } else if(type === 'online') {
       setOnline(!online);
+      ticketType = ticket.filter(item => item.name === type);
+    }else {
+      setPresentialWihtHotel(!presentialWithHotel);
+      setPresentialWihtoutHotel(false);
+      ticketType = ticket.filter(item => item.name === 'presencial' && item.includesHotel === true );
     }
+    setTicket(ticketType);
   };
-  
-  useEffect(() => {
-    if(ticket) {
-      setTickets({ ticket });
+  async function submit() {
+    try {
+      const body = { ticketTypeId: Ticket[0].id };
+      await postTicket(body);
+    } catch (error) {
+      toast('Não foi possível reservar o ingresso');
     }
-  }, [ticketLoading]);
+  }
   
   return (
     <>
       <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
       <SubTitle variant='h6'>Primeiro, escolha sua modalidade de ingresso</SubTitle>
       <TicketTypeBox>
-        <Card onClick={() => selectTicket('presential')} primary={presential} >
+        <Card onClick={() => selectTicket('presencial')} primary={presential} >
           <TicketType >{ticket === null ? <> </> : ticket[1].name}</TicketType >
           <TicketPrice>R${ticket === null ? <> </> : ticket[1].price }</TicketPrice>
         </Card>
@@ -40,15 +63,25 @@ export default function Payment() {
         <>  
           <SubTitle variant='h6'>Ótimo! Agora escolha sua modalidade de hospedagem</SubTitle>
           <TicketTypeBox>
-            <Card>
+            <Card  onClick={() => selectTicket('no hotel')} primary={presentialWithoutHotel} >
               <TicketType >Sem Hotel</TicketType >
               <TicketPrice>+ R$ 0</TicketPrice>
             </Card>
-            <Card>
-              <TicketType >Com Hotel</TicketType >
+            <Card  onClick={() => selectTicket()} primary={presentialWithHotel}>
+              <TicketType>Com Hotel</TicketType >
               <TicketPrice>+ R$ 350</TicketPrice>
             </Card>
           </TicketTypeBox>
+          {presentialWithHotel || presentialWithoutHotel ? (
+            <>
+              <SubTitle variant='h6'>Fechado! O total ficou em <strong> R$ {Ticket[0].price}.</strong> agora é so confirmar</SubTitle>
+              <Button onClick={submit}>RESERVAR INGRESSO</Button>
+            </>
+          ):(
+            <>
+            </>
+          )}
+          
         </>
       ): (
         <>
