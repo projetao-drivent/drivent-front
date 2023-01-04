b35a3bd3962c96196019d3d2f19c7fb32554cb90
daniel <daniel.bnh13@gmail.com>
2023-01-04 18:44:07 -0300

    docs: mensagem de aviso na pagina atividade

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,27 @@
+import * as paymentApi from '../../../services/paymentApi';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import useToken from '../../../hooks/useToken';
+import {
+  NoHotelMsg
+} from '../../../components/Dashboard/Activities/index.js';
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const [ tela, setTela ] = useState(true);
+  const [ tela2, setTela2 ] = useState(true);
+  const token = useToken();
+  useEffect(async() => {
+    async function confirmPayment() {
+      const res= await paymentApi.getTicket(token);
+      if(res) {
+        if(res.status==='PAID')  {
+          setTela(false);
+        }
+        if(res.TicketType.isRemote) {
+          setTela2(false);
+        }
+      }
+    }
+    await confirmPayment();
+  }, []);
+  return(tela ? <NoHotelMsg>Você precisa ter confirmado pagamento antes de fazer a escolha de atividades</NoHotelMsg>: <> {tela2 ? <>Atividades: Em breve!</>:<NoHotelMsg>Sua modalidade de ingresso nao necessita escolher atividades. Você terá acesso a todas as atividades.</NoHotelMsg>}</>); 
 }
