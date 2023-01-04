a52c6d6eb4a6050013deb8ae0f9caec7c4754d2d
daniel <daniel.bnh13@gmail.com>
2023-01-04 14:31:13 -0300

    docs: cria  mensagens na tela de atividades para casos especificos

---
+++ src/components/Dashboard/Activities/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+export const NoHotelMsg = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  text-align: center;
+  color: #8e8e8e;
+  width: 35vw;
+  height: 30vh;
+  margin: 30vh auto;
+`;
